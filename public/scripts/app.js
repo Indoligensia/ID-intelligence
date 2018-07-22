f64ecd723c1fa4ae8abbd8201ef4cd85fa50ad0e
alramadona <alramadona@outlook.com>
2018-07-22 17:33:10 +0300

    Initiate ReactJS

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// JSX - JavaScript XML
+var template = React.createElement(
+  'h1',
+  null,
+  'ID-intelligence'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+// JSX - JavaScript XML
+var template = <h1>ID-intelligence</h1>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);

c4dc16b4eada52e541fa1267a8333e96e0214e70
alramadona <alramadona@outlook.com>
2018-07-28 17:39:57 +0300

    show how confused I am

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
@@ -67,6 +68,9 @@ class App extends Component {
             'infowindow': '',
             'prevmarker': ''
         };
+        this.initMap = this.initMap.bind(this);
+        this.openInfoWindow = this.openInfoWindow.bind(this);
+        this.closeInfoWindow = this.closeInfoWindow.bind(this);
     }
 
     handleToggle = () => this.setState({open: !this.state.open});
@@ -74,6 +78,117 @@ class App extends Component {
     showFoo = () => {
         this.setState({show: 'foo', open: false });
     };
+
+    componentDidMount() {
+        window.initMap = this.initMap;
+        loadMapJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyBXCJFXLQPUD5i3eglS9sfdzCTkadtx4Cs&callback=initMap')
+    }
+
+    initMap() {
+        var self = this;
+        var mapview = document.getElementById('map');
+        mapview.style.height = window.innerHeight + "px";
+        var map = new window.google.maps.Map(mapview, {
+            center: {lat: -7.797068, lng: 110.370529},
+            zoom: 14,
+            mapTypeControl: false
+        });
+
+        var InfoWindow = new window.google.maps.InfoWindow({});
+
+        window.google.maps.event.addListener(InfoWindow, 'closeclick', function () {
+            self.closeInfoWindow();
+        });
+
+        this.setState({
+            'map': map,
+            'infowindow': InfoWindow
+        });
+
+        window.google.maps.event.addDomListener(window, "resize", function () {
+            var center = map.getCenter();
+            window.google.maps.event.trigger(map, "resize");
+            self.state.map.setCenter(center);
+        });
+
+        window.google.maps.event.addListener(map, 'click', function () {
+            self.closeInfoWindow();
+        });
+
+        var datlocations = [];
+        this.state.datlocations.forEach(function (location) {
+            var longname = location.name + ' - ' + location.type;
+            var marker = new window.google.maps.Marker({
+                position: new window.google.maps.LatLng(location.latitude, location.longitude),
+                animation: window.google.maps.Animation.DROP,
+                map: map
+            });
+
+            marker.addListener('click', function () {
+                self.openInfoWindow(marker);
+            });
+
+            location.longname = longname;
+            location.marker = marker;
+            location.display = true;
+            datlocations.push(location);
+        });
+        this.setState({
+            'datlocations': datlocations
+        });
+    }
+
+    openInfoWindow(marker) {
+        this.closeInfoWindow();
+        this.state.infowindow.open(this.state.map, marker);
+        marker.setAnimation(window.google.maps.Animation.BOUNCE);
+        this.setState({
+            'prevmarker': marker
+        });
+        this.state.infowindow.setContent('Loading Data...');
+        this.state.map.setCenter(marker.getPosition());
+        this.state.map.panBy(0, -200);
+        this.getMarkerInfo(marker);
+    }
+
+    getMarkerInfo(marker) {
+        var self = this;
+        var clientId = "LG0GCJH0SBNPTAOEJCSFKZGNAGBPZYGOQ3OYD1H0C1SSDBPW";
+        var clientSecret = "N5UWB0SD3UJCYKPZR24RNKBJYSOJPRLHMRMCTFHB4BRDACJ5";
+        var url = "https://api.foursquare.com/v2/venues/search?client_id=" + clientId + "&client_secret=" + clientSecret + "&v=20130815&ll=" + marker.getPosition().lat() + "," + marker.getPosition().lng() + "&limit=1";
+        fetch(url)
+            .then(
+                function (response) {
+                    if (response.status !== 200) {
+                        self.state.infowindow.setContent("Sorry data can't be loaded");
+                        return;
+                    }
+
+                    response.json().then(function (data) {
+                        var location_data = data.response.venues[0];
+                        var verified = '<b>Verified Location: </b>' + (location_data.verified ? 'Yes' : 'No') + '<br>';
+                        var checkinsCount = '<b>Number of CheckIn: </b>' + location_data.stats.checkinsCount + '<br>';
+                        var usersCount = '<b>Number of Users: </b>' + location_data.stats.usersCount + '<br>';
+                        var tipCount = '<b>Number of Tips: </b>' + location_data.stats.tipCount + '<br>';
+                        var readMore = '<a href="https://foursquare.com/v/'+ location_data.id +'" target="_blank">Read More on Foursquare Website</a>'
+                        self.state.infowindow.setContent(checkinsCount + usersCount + tipCount + verified + readMore);
+                    });
+                }
+            )
+            .catch(function (err) {
+                self.state.infowindow.setContent("Sorry data can't be loaded");
+            });
+    }
+
+    closeInfoWindow() {
+        if (this.state.prevmarker) {
+            this.state.prevmarker.setAnimation(null);
+        }
+        this.setState({
+            'prevmarker': ''
+        });
+        this.state.infowindow.close();
+}
 
     render() {
         let content = null;
@@ -88,11 +203,12 @@ class App extends Component {
         }
 
         return (
+            <MuiThemeProvider>
             <div>
 
                 <AppBar
                     iconClassNameRight="muidocs-icon-navigation-expand-more"
-                    title="ID-intelligence"
+                    title="ID-intelligence: Yogyakarta"
                     onLeftIconButtonClick={this.handleToggle}
                 />
 
@@ -108,15 +224,27 @@ class App extends Component {
 
                 <Paper style={paperStyle} zDepth={5}>
                     <Toolbar style={{"justifyContent": "center"}}>
-                        <ToolbarTitle text="Yogyakarta"/>
+                        <ToolbarTitle text="hospital"/>
                     </Toolbar>
                     {content}
                     <p>Click the icon on the AppBar to open the Drawer and then pick a menu item. The text above should change.</p>
                 </Paper>
 
             </div>
+            </MuiThemeProvider>
         );
     }
 }
 
 export default App;
+
+function loadMapJS(src) {
+    var ref = window.document.getElementsByTagName("script")[0];
+    var script = window.document.createElement("script");
+    script.src = src;
+    script.async = true;
+    script.onerror = function () {
+        document.write("Google Maps can't be loaded");
+    };
+    ref.parentNode.insertBefore(script, ref);
+}
